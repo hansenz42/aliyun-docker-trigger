25d576388795ede9f64aecc86162181d23270bde
Hansen <hansen.zheng@hotmail.com>
2025-03-19 14:58:14 +0800

    特性：更改默认模式为 compose

--- config.js
+++ config.js
@@ -13,6 +13,6 @@ module.exports = {
 		},
 		subscribers: process.env.EMAIL_SUBSCRIBERS,
 	},
-	dockerMode: process.env.DOCKER_MODE | 'swarm',
+	dockerMode: process.env.DOCKER_MODE | 'compose',
 	serverName: process.env.SERVER_NAME | 'aliyun-docker-trigger',
 }
